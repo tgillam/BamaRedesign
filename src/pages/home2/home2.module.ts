83b80b0e609985625af158137fbd22e0ee7166d1
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2018-09-26 20:45:36 -0500

    Fixed declaration error

--- src/pages/home2/home2.module.ts
+++ src/pages/home2/home2.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { Home2Page } from './home2';
+import { HomePage2 } from './home2';
 
 @NgModule({
   declarations: [
-    Home2Page,
+    HomePage2,
   ],
   imports: [
-    IonicPageModule.forChild(Home2Page),
+    IonicPageModule.forChild(HomePage2),
   ],
 })
-export class Home2PageModule {}
+export class HomePage2Module {}
