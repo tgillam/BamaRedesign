f78c4a4994457c6492385e18e21afb3bdea58acf
Ethan Johnson <ekjohnson4@crimson.ua.edu>
2018-10-23 23:53:08 -0500

    Added slides to home and made logo quick link

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the Events page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -22,4 +16,7 @@ export class Events {
     console.log('ionViewDidLoad Events');
   }
 
+  goToPage() {
+    this.navCtrl.setRoot(HomePage);
+  }
 }
